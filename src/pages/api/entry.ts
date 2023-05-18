8ef5b7026b9b69f862b88156aa8ffa7328db19a5
abraham <aaabrham421@gmail.com>
2023-05-18 16:45:22 +0300

    added basic CRUD APIs for journal, entry and user see:JAP-21

---
+++ src/pages/api/entry.ts
@@ -0,0 +1,34 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "GET") {
+    const users = await prisma.entry.findMany();
+    res.status(200).json(users);
+  } else if (req.method === "POST") {
+    const postData = req.body;
+    const user = await prisma.entry.create({
+      data: { ...postData },
+    });
+    res.status(201).json(user);
+  } else if (req.method === "PUT") {
+    const putData = req.body;
+    const user = await prisma.entry.update({
+      where: { id: putData.id },
+      data: { ...putData },
+    });
+    res.status(200).json(user);
+  } else if (req.method === "DELETE") {
+    const { id } = req.query;
+    await prisma.entry.delete({
+      where: { id: id?.toString() },
+    });
+    res.status(204).end();
+  } else {
+    res.status(405).json({ message: "Method Not Allowed" });
+  }
+}
