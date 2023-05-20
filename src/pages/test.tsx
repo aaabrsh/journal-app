be6a36ee760d96ade40ae62d43f43f3e9630777f
abraham <aaabrham421@gmail.com>
2023-05-20 12:02:25 +0300

    two working wysiwyg components - with bugs see:JAP-20
    needs a way to figure out how to load the libraries on the browser

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -1,24 +1,6 @@
-// import React, { useState } from "react";
-// import ReactQuill from "react-quill";
-// import "react-quill/dist/quill.snow.css";
-
-// export default function Index() {
-//   const [value, setValue] = useState("");
-
-//   function handleChange(text: string) {
-//     console.log(text);
-//     setValue(text);
-//   }
-//   return (
-//     <>
-//     <div></div>
-//       <ReactQuill theme="snow" value={value} onChange={handleChange} modules={} />
-//     </>
-//   );
-// }
-
-import React, { useEffect, useState } from "react";
-// import { CKEditor } from "@ckeditor/ckeditor5-react";
+import React, { useState, useEffect } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 import dynamic from "next/dynamic";
 const CKEditor = dynamic(
@@ -27,38 +9,63 @@ const CKEditor = dynamic(
     ssr: false,
   }
 );
-function Editor() {
+
+export default function Index() {
+  const [value, setValue] = useState("");
+  const [value1, setValue1] = useState("");
+
   let [loaded, setLoaded] = useState(false);
 
   useEffect(() => {
     setLoaded(true);
   }, []); // run on mounting
 
-  if (loaded) {
-    return (
-      <CKEditor
-        editor={ClassicEditor}
-        data="<p>Hello from CKEditor 5!</p>"
-        onReady={(editor) => {
-          // You can store the "editor" and use when it is needed.
-          console.log("Editor is ready to use!", editor);
-        }}
-        onChange={(event, editor: any) => {
-          // do something when editor's content changed
-          const data = editor.getData();
-          console.log({ event, editor, data });
-        }}
-        onBlur={(event, editor) => {
-          console.log("Blur.", editor);
-        }}
-        onFocus={(event, editor) => {
-          console.log("Focus.", editor);
-        }}
-      />
-    );
-  } else {
-    return <h2> Editor is loading </h2>;
+  function handleChange(text: string) {
+    console.log(text);
+    setValue(text);
   }
-}
 
-export default Editor;
+  function showValue() {
+    console.log("value of the first element is ", value);
+    console.log("value of the second element is ", value1);
+  }
+
+  return (
+    <>
+      <div></div>
+      <ReactQuill
+        theme="snow"
+        value={value}
+        onChange={handleChange}
+        // modules={}
+      />
+      <div>Second componenet</div>
+      {loaded ? (
+        <CKEditor
+          editor={ClassicEditor}
+          data="<p>Hello from CKEditor 5!</p>"
+          onReady={(editor) => {
+            // You can store the "editor" and use when it is needed.
+            console.log("Editor is ready to use!", editor);
+          }}
+          onChange={(event, editor: any) => {
+            // do something when editor's content changed
+            const data = editor.getData();
+            setValue1(data);
+            console.log({ event, editor, data });
+          }}
+          onBlur={(event, editor) => {
+            console.log("Blur.", editor);
+          }}
+          onFocus={(event, editor) => {
+            console.log("Focus.", editor);
+          }}
+        />
+      ) : (
+        <div>Editor is Loading</div>
+      )}
+
+      <button onClick={showValue}></button>
+    </>
+  );
+}
